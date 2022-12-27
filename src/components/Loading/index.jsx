3067504eaab304bc8438bec8e8bd01e10e43bb03
kienlu <kienlu2000@gmail.com>
2022-12-27 12:03:44 +0700

    update config vercel

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export const Loading = () => {
   return (
-    <div className="animation-loading">
-      <div className="box box1"></div>
-      <div className="box box2"></div>
-    </div>
+    // <div className="animation-loading">
+    //   <div className="box box1"></div>
+    //   <div className="box box2"></div>
+    // </div>
+    <div>. . . Loading . . .</div>
   );
 };
